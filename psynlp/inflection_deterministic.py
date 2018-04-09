075c0880d6b4801b3ddc5b6c4c36cc4a95485e8c
Athitya <athityakumar@gmail.com>
2018-04-09 15:56:56 +0530

    Adds changes to inflection_deterministic test

--- psynlp/inflection_deterministic.py
+++ psynlp/inflection_deterministic.py
@@ -14,14 +14,14 @@ def fetch_accuracy(language='english', quality='high'):
     total = correct = 0
 
     for (source, metadata, expected_dest) in testing_data:
-        print("Guessing for {} + {} now".format(source, metadata))
         scores = []
         if metadata not in pac:
-            print("Skipping word")
+            print("Skipping {}".format(source))
             continue
+
         concept, cluster, _ = pac[metadata]
         if not cluster:
-            print("Skipping word")
+            print("Skipping {}".format(source))
             continue
 
         for (antecedent_attrs, consequent_attrs) in cluster:
@@ -48,16 +48,71 @@ def fetch_accuracy(language='english', quality='high'):
             operations = concept.objects_intent(set(cluster_words))
 
         computed_dest = inflect(source, operations)
+
         if computed_dest == expected_dest:
             correct += 1
             print("{} + {}: Expected and found {}".format(source, metadata, computed_dest))
         else:
             print("{} + {}: Expected {} but found {}".format(source, metadata, expected_dest, computed_dest))
-        print("due to {} with score {}".format(closest_word, min_score))
+        if closest_word:
+            print("due to {} with score {}".format(closest_word, min_score))
         total += 1
 
     if total == 0:
         total = 1
     accuracy = 100*float(correct) / total
-    print(accuracy)
     return accuracy
+
+    # for (source, metadata, expected_dest) in testing_data:
+    #     scores = []
+    #     computed_dest = closest_word = None
+
+    #     if metadata not in pac:
+    #         print("Skipping {}".format(source))
+    #         computed_dest = source
+
+    #     if computed_dest is None:
+    #         concept, cluster, _ = pac[metadata]
+    #         if not cluster:
+    #             print("Skipping {}".format(source))
+    #             computed_dest = source
+
+    #         if computed_dest is None:
+    #             for (antecedent_attrs, consequent_attrs) in cluster:
+    #                 ostia = OSTIA(consequent_attrs)
+    #                 scores.append((consequent_attrs, ostia.matches_any_path(source)))
+
+    #             scores = sorted(scores, key=lambda x: x[1][0])
+    #             cluster_words, (min_score, closest_word) = scores[0]
+    #             just_scores = [s[0] for _, s in scores]
+
+    #             if just_scores.count(min_score) == 1:
+    #                 operations = concept.objects_intent(set(cluster_words))
+    #             else:
+    #                 max_operations = 0
+    #                 index_of_min_score = 0
+    #                 for i, s in enumerate(scores[0:just_scores.count(min_score)]):
+    #                     this_cluster, (score, _) = s
+    #                     operations = concept.objects_intent(set(this_cluster))
+    #                     if len(operations) > max_operations:
+    #                         max_operations = len(operations)
+    #                         cluster_words = this_cluster
+    #                         index_of_min_score = i
+    #                 closest_word = scores[index_of_min_score][1][1]
+    #                 operations = concept.objects_intent(set(cluster_words))
+
+    #             computed_dest = inflect(source, operations)
+
+    #     if computed_dest == expected_dest:
+    #         correct += 1
+    #         print("{} + {}: Expected and found {}".format(source, metadata, computed_dest))
+    #     else:
+    #         print("{} + {}: Expected {} but found {}".format(source, metadata, expected_dest, computed_dest))
+    #     if closest_word:
+    #         print("due to {} with score {}".format(closest_word, min_score))
+    #     total += 1
+
+    # if total == 0:
+    #     total = 1
+    # accuracy = 100*float(correct) / len(testing_data)
+    # return accuracy
