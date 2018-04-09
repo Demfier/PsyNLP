9c41dd63e67a05affbfd90b9cb2a870d120c466a
Athitya <athityakumar@gmail.com>
2018-04-09 23:51:43 +0530

    Starts adding option for --verbose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 from os import listdir
+import psynlp.helpers.globals as g
+import builtins as __builtin__
 
 parser = argparse.ArgumentParser(
     description='Runs one of the pipeline scripts, for a given language and quality.')
@@ -9,8 +11,10 @@ parser.add_argument('-l', '--language', default='english',
                     help='Name of the language (Default: english)')
 parser.add_argument('-q', '--quality', default='low',
                     help='Size of the training data (Default: low)')
-
+parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False, help='Prints verbose output if specified')
 args = parser.parse_args()
+print =  __builtin__.print if args.verbose else lambda *a, **k: None
+
 PIPELINES = [f.rstrip('.py') for f in listdir(
     'psynlp/pipelines') if f.endswith('.py')]
 LANGUAGES = [f.split('-train-high')[0]
